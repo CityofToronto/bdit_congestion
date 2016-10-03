7294022f2e3f0bc8488c5b359fd49176d6cf6488
Raphael Dumas <raphael.a.dumas@gmail.com>
2016-10-03 18:14:55 -0400

    More performance report queries
    Including a Jupyter Notebook for shiny graph creations.

---
+++ sql/corridors/corridor_yoy_med_tt.sql
@@ -0,0 +1,13 @@
+﻿SELECT corridor, direction, amq2.tt_med - amq1.tt_med AS "AM Peak Travel Time Difference", pmq2.tt_med - pmq1.tt_med AS "PM Peak Travel Time Difference"
+  FROM key_corridor_perf amq1
+  INNER JOIN key_corridor_perf amq2 USING (corridor, direction, daytype)
+  INNER JOIN key_corridor_perf pmq1 USING (corridor, direction, daytype)
+  INNER JOIN key_corridor_perf pmq2 USING (corridor, direction, daytype)
+  WHERE daytype = 'Midweek'  
+  AND amq1.quarter = '2014-04-01' AND amq1.period = 'AMPK'
+  AND amq2.quarter = '2016-04-01' AND amq2.period = 'AMPK'
+  AND pmq1.quarter = '2014-04-01' AND pmq1.period = 'PMPK'
+  AND pmq2.quarter = '2016-04-01' AND pmq2.period = 'PMPK'
+ORDER BY corridor, direction
+
+  ;
--- sql/corridors/filtered_quarter_percentiles.sql
+++ sql/corridors/filtered_quarter_percentiles.sql
@@ -1,5 +1,5 @@
-﻿/*Step 3: Filter by daytype and timeperiod*/
-SELECT quarter::date, corridor, direction, period, tt_med as median_tt, tt_95th - tt_med as buffer_time
+﻿SELECT quarter::date, corridor, direction, period, SUM(tt_med) as median_tt, SUM(tt_95th) - SUM(tt_med) as buffer_time
 FROM key_corridor_perf 
 WHERE daytype = 'Midweek' and period IN ('AMPK','PMPK')
+GROUP BY quarter, corridor, direction, period
 ORDER BY corridor, direction, period, quarter
