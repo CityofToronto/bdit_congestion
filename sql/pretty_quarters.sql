a9766761fb138d5b9ca7c292d818a67d733bd2b4
Raphael Dumas <raphael.a.dumas@gmail.com>
2016-10-06 18:52:04 -0400

    Function for prettifying corridor strings

---
+++ sql/pretty_quarters.sql
@@ -0,0 +1,7 @@
+﻿CREATE OR REPLACE FUNCTION pretty_quarter(quarter DATE) 
+RETURNS char(7) 
+AS $$
+BEGIN 
+	RETURN to_char(quarter,'YYYY')||' '||'Q'||date_part('quarter',quarter);
+END;
+$$LANGUAGE plpgsql
