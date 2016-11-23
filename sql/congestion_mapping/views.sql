c1c8ec79792d2a2d01be128fb50c0b3e29312985
Raphael Dumas <raphael.a.dumas@gmail.com>
2016-11-23 15:58:22 -0500

    Adding more columns to view
    Also can't limit within view if year `WHERE` clause is to be used
    after...

--- sql/congestion_mapping/views.sql
+++ sql/congestion_mapping/views.sql
@@ -1,13 +1,16 @@
-﻿CREATE OR REPLACE VIEW congestion.least_reliable_annual_50 AS
+﻿CREATE OR REPLACE VIEW congestion.least_reliable_annual AS
 SELECT row_number() OVER (PARTITION BY agg_period ORDER BY bti DESC) AS "Rank",
+ street_name AS "Street",
  direction AS "Dir", 
- bti as "Buffer Time Index",
+ from_to AS "From - To",
+ bti as "Buffer Time Index", 
+ agg_period AS "Year",
  geom,
- agg_period
+ gid
 
 FROM congestion.metrics 
 INNER JOIN congestion.aggregation_levels USING (agg_id)
 INNER JOIN gis.inrix_tmc_tor USING (tmc)
+INNER JOIN gis.tmc_from_to_lookup USING (tmc)
 WHERE agg_level = 'year'
 ORDER BY bti DESC
-LIMIT 50
