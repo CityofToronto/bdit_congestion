630b628799850b8ac8e0729b3afbb99204307046
Raphael Dumas <raphael.a.dumas@gmail.com>
2016-11-23 17:33:49 -0500

    Adding am/pm reliability views

--- sql/congestion_mapping/views.sql
+++ sql/congestion_mapping/views.sql
@@ -1,4 +1,4 @@
-﻿CREATE OR REPLACE VIEW congestion.least_reliable_annual AS
+﻿CREATE OR REPLACE VIEW congestion.least_reliable_annual_am AS
 SELECT row_number() OVER (PARTITION BY agg_period ORDER BY bti DESC) AS "Rank",
  street_name AS "Street",
  direction AS "Dir", 
@@ -12,5 +12,22 @@ FROM congestion.metrics
 INNER JOIN congestion.aggregation_levels USING (agg_id)
 INNER JOIN gis.inrix_tmc_tor USING (tmc)
 INNER JOIN gis.tmc_from_to_lookup USING (tmc)
-WHERE agg_level = 'year'
+WHERE agg_level = 'year' AND timeperiod = timerange('08:00'::TIME, '09:00'::TIME)
+ORDER BY bti DESC;
+
+CREATE OR REPLACE VIEW congestion.least_reliable_annual_pm AS
+SELECT row_number() OVER (PARTITION BY agg_period ORDER BY bti DESC) AS "Rank",
+ street_name AS "Street",
+ direction AS "Dir", 
+ from_to AS "From - To",
+ bti as "Buffer Time Index", 
+ agg_period AS "Year",
+ geom,
+ gid
+
+FROM congestion.metrics 
+INNER JOIN congestion.aggregation_levels USING (agg_id)
+INNER JOIN gis.inrix_tmc_tor USING (tmc)
+INNER JOIN gis.tmc_from_to_lookup USING (tmc)
+WHERE agg_level = 'year' AND timeperiod = timerange('17:00'::TIME, '18:00'::TIME)
 ORDER BY bti DESC
