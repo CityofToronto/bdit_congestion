9eb584930bc04a2ee619d99843c45984f63aa188
Raphael Dumas <raphael.a.dumas@gmail.com>
2016-12-13 10:09:37 -0500

    Separated parsing utils to new file
    parse_args and other helper functions migrated to utils file
    Fixes #5 by adding {metric} to the SQL string to be replaced by a
    function

--- python/congestion_mapping/map_metric.py
+++ python/congestion_mapping/map_metric.py
@@ -44,9 +44,9 @@ import sys
 import logging
 import re
 from datetime import time
-
+from parsing_utils import parse_args, _validate_multiple_yyyymm_range
 SQLS = {#'month':"",
-        'year':"""(SELECT row_number() OVER (PARTITION BY metrics.agg_period ORDER BY metrics.bti DESC) AS "Rank",
+        'year':"""(SELECT row_number() OVER (PARTITION BY metrics.agg_period ORDER BY metrics.{metric} DESC) AS "Rank",
                     tmc_from_to_lookup.street_name AS "Street",
                     inrix_tmc_tor.direction AS "Dir",
                     tmc_from_to_lookup.from_to AS "From - To",
@@ -61,201 +61,9 @@ SQLS = {#'month':"",
                     JOIN gis.tmc_from_to_lookup USING (tmc)
                   WHERE inrix_tmc_tor.sum_miles > 0.124274 AND aggregation_levels.agg_level = 'year'
                   AND metrics.timeperiod = {timeperiod} AND metrics.agg_period = {agg_period}::DATE
-                  ORDER BY metrics.bti DESC LIMIT 50)"""#,
+                  ORDER BY metrics.{metric} DESC LIMIT 50)"""#,
 #        'quarter':''
 }
-
-def parse_args(args, prog = None, usage = None):
-    '''Parser for the command line arguments
-    
-    Args:
-        sys.argv[1]: command line arguments
-        prog: alternate program name, FOR TESTING
-        usage: alternate usage message, to suppress FOR TESTING
-        
-    Returns:
-        dictionary of parsed arguments
-    '''
-    PARSER = argparse.ArgumentParser(description='Produce maps of congestion metrics (tti, bti) for '
-                                                 'different aggregation periods, timeperiods, and '
-                                                 'aggregation levels', prog=prog, usage=usage)
-
-    PARSER.add_argument('Metric', choices=['b', 't'], nargs='+',
-                        help="Map either Buffer Time Index, Travel"
-                        "Time Index or both e.g. b, t, or 'b t'."
-                        "Make sure to space arguments")
-
-    PARSER.add_argument("Aggregation", choices=['year', 'quarter', 'month'],
-                        help="Aggregation level to be used")
-
-    PARSER.add_argument("-r", "--range", nargs=2, action='append',
-                        help="Range of months (YYYYMM) to operate over"
-                        "from startdate to enddate. Accepts multiple pairs",
-                        metavar=('YYYYMM', 'YYYYMM'), required=True)
-
-    TIMEPERIOD = PARSER.add_mutually_exclusive_group(required=True)
-    TIMEPERIOD.add_argument("-p", "--timeperiod", nargs='+', type=int,
-                            help="Timeperiod of aggregation, use 1 arg for 1 hour or 2 args for a range")
-    TIMEPERIOD.add_argument("-i","--hours_iterate", nargs=2, type=int,
-                            help="Hours to iterate over")
-
-    PARSER.add_argument("-d", "--dbsetting",
-                        default='default.cfg',
-                        help="Filename with connection settings to the database"
-                        "(default: opens %(default)s)")
-    PARSER.add_argument("-t", "--tablename",
-                        default='congestion.metrics',
-                        help="Table containing metrics %(default)s")
-    parsed_args = PARSER.parse_args(args)
-
-    if parsed_args.timeperiod and len(parsed_args.timeperiod) > 2:
-        PARSER.error('--timeperiod takes one or two arguments')
-    return parsed_args
-
-def get_yyyymmdd(yyyy, mm, **kwargs):
-    '''Combine integer yyyy and mm into a string date yyyy-mm-dd.'''
-    
-    if 'dd' not in kwargs:
-        dd = '01'    
-    elif kwargs['dd'] >= 10:
-        dd = str(kwargs['dd'])
-    elif kwargs['dd'] < 10:
-        dd = '0'+str(kwargs['dd'])
-
-    if mm < 10:
-        return str(yyyy)+'-0'+str(mm)+'-01'
-    else:
-        return str(yyyy)+'-'+str(mm)+'-01'
-
-def _validate_yyyymm_range(yyyymmrange, agg_level):
-    '''Validate the two yyyymm command line arguments provided
-
-    Args:
-        yyyymmrange: List containing a start and end year-month in yyyymm format
-        agg_level: the aggregation level, determines number of months each 
-            period spans
-
-    Returns:
-        A dictionary with the processed range like {'yyyy':range(mm1,mm2+1)}
-
-    Raises:
-        ValueError: If the values entered are incorrect
-    '''
-
-    if agg_level not in SQLS:
-        raise ValueError('Aggregation level: {agg_level} not implemented'.format(agg_level=agg_level))
-    elif agg_level == 'month':
-        step = 1
-    elif agg_level == 'quarter':
-        step = 3
-
-    if len(yyyymmrange) != 2:
-        raise ValueError('{yyyymmrange} should contain two YYYYMM arguments'
-                         .format(yyyymmrange=yyyymmrange))
-
-    regex_yyyymm = re.compile(r'20\d\d(0[1-9]|1[0-2])')
-    yyyy, mm = [], []
-    years = {}
-
-    for yyyymm in yyyymmrange:
-        if regex_yyyymm.fullmatch(yyyymm):
-            if agg_level == 'year' and int(yyyymm[-2:]) != 1:
-                raise ValueError('For annual aggregation, month must be 01 not {yyyymm}'
-                                 .format(yyyymm=yyyymm))
-            elif agg_level == 'quarter' and (int(yyyymm[-2:]) % 3) != 1:
-                raise ValueError('For quarterly mapping, month must be in [1,4,7,10] not {yyyymm}'
-                                 .format(yyyymm=yyyymm))
-            yyyy.append(int(yyyymm[:4]))
-            mm.append(int(yyyymm[-2:]))
-        else:
-            raise ValueError('{yyyymm} is not a valid year-month value of format YYYYMM'
-                             .format(yyyymm=yyyymm))
-
-    if yyyy[0] > yyyy[1] or (yyyy[0] == yyyy[1] and mm[0] > mm[1]):
-        raise ValueError('Start date {yyyymm1} after end date {yyyymm2}'
-                         .format(yyyymm1=yyyymmrange[0], yyyymm2=yyyymmrange[1]))
-    
-    if agg_level == 'year':
-        #Only add January for each year to be mapped
-        if yyyy[0] == yyyy[1]:
-            years[yyyy[0]] = 1
-        else:
-            for year in range(yyyy[0], yyyy[1]+1):
-                years[year] = 1
-    else: 
-        #Iterate over years and months with specified aggregation step 
-        #(month or quarter)
-        if yyyy[0] == yyyy[1]:
-            years[yyyy[0]] = range(mm[0], mm[1]+1, step)
-        else:
-            for year in range(yyyy[0], yyyy[1]+1):
-                if year == yyyy[0]:
-                    years[year] = range(mm[0], 13, step)
-                elif year == yyyy[1]:
-                    years[year] = range(1, mm[1]+1, step)
-                else:
-                    years[year] = range(1, 13, step)
-
-    return years
-
-def _validate_multiple_yyyymm_range(years_list, agg_level):
-    '''Validate a list of pairs of yearmonth strings
-    
-    Takes one or more lists like ['YYYYMM','YYYYMM'] and passes them to 
-    _validate_yyyymm_range then merges them back into a dictionary of
-    years[YYYY] = [month1, month2, etc]
-    
-    Args: 
-        years_list: a list of lists of yyyymm strings
-        agg_level: the aggregation level, determines number of months each 
-            period spans
-    
-    Raises:
-        ValueError: If the values entered are incorrect
-    
-    Returns:
-        a dictionary of years[YYYY] = [month1, month2, etc]
-    '''
-    years = {}
-    if len(years_list) == 1:
-        years = _validate_yyyymm_range(years_list[0], agg_level)
-    else:
-        for yearrange in years_list:
-            years_to_add = _validate_yyyymm_range(yearrange, agg_level)
-            for year_to_add in years_to_add:
-                if year_to_add not in years:
-                    years[year_to_add] = years_to_add[year_to_add]
-                else:
-                    years[year_to_add] = set.union(set(years_to_add[year_to_add]),
-                                                   set(years[year_to_add]))
-    return years
-
-def _get_timerange(time1, time2):
-    '''Validate provided times and create a timerange string to be inserted into PostgreSQL
-    
-    Args:
-        time1: Integer first hour
-        time2: Integer second hour
-        
-    Returns:
-        String representation creating a PostgreSQL timerange object
-    '''
-    if time1 == time2:
-        raise ValueError('2nd time parameter {time2} must be at least 1 hour after first parameter {time1}'.format(time1=time1, time2=time2))
-        
-    starttime = time(int(time1))
-    
-    #If the second time is 24, aka midnight, replace with maximum possible time for the range
-    if time2 == 24:
-        endtime = time.max
-    else:
-        endtime = time(int(time2))
-        
-    if starttime > endtime:
-        raise ValueError('start time {starttime} after end time {endtime}'.format(starttime=starttime, endtime=endtime))
-    
-    return 'timerange(\'{starttime}\'::time, \'{endtime}\'::time)'.format(starttime=starttime.isoformat(),
-                                                                          endtime=endtime.isoformat())
 
 
 def _new_uri(dbset):
@@ -320,7 +128,7 @@ if __name__ == '__main__':
                 hour2 = hour1 + 1 if ARGS.hours_iterate else ARGS.timeperiod[1] 
                 timerange = _get_timerange(hour1, hour2)
                 layername = year + month + 'h' + hour1 + ARGS.agg_level
-                _get_agg_layer(URI, agg_level = ARGS.agg_level,
+                layer = _get_agg_layer(URI, agg_level = ARGS.agg_level,
                                agg_period = yyyymmdd,
                                timeperiod = timerange,
                                layername=layername)
