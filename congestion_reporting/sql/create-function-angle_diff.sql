ae21ebe03c916f40e32c20c5633d01a18a51a0fd
Aakash Harpalani <aharpal@toronto.ca>
2017-06-12 17:33:28 -0400

    Conflation line-based functions

---
+++ congestion_reporting/sql/create-function-angle_diff.sql
@@ -0,0 +1,27 @@
+﻿CREATE OR REPLACE FUNCTION angle_diff(link1 geometry, link2 geometry) RETURNS double precision
+	AS
+$BODY$
+DECLARE
+	angle double precision;
+	angle1 double precision;
+	angle2 double precision;
+BEGIN
+	angle1 := degrees(ST_Azimuth(ST_StartPoint(ST_Transform(ST_GeometryN(link1,1),32190)),ST_EndPoint(ST_Transform(ST_GeometryN(link1,ST_NumGeometries(link1)),32190))));
+	angle2 := degrees(ST_Azimuth(ST_ClosestPoint(ST_Transform(link2,32190),ST_StartPoint(ST_Transform(ST_GeometryN(link1,1),32190))),
+		ST_ClosestPoint(ST_Transform(link2,32190),ST_EndPoint(ST_Transform(ST_GeometryN(link1,ST_NumGeometries(link1)),32190)))
+		));
+
+	IF (angle1 > 180) THEN
+		angle1 := angle1 - 180;
+	END IF;
+
+	IF (angle2 > 180) THEN
+		angle2 := angle2 - 180;
+	END IF;
+
+	angle := abs(angle1 - angle2);
+	RETURN angle;
+END;
+$BODY$
+  LANGUAGE plpgsql IMMUTABLE
+  COST 100;
---
+++ congestion_reporting/sql/create-function-line_dist.sql
@@ -0,0 +1,39 @@
+﻿-- Function: line_dist(geometry, geometry)
+
+-- DROP FUNCTION line_dist(geometry, geometry);
+
+CREATE OR REPLACE FUNCTION line_dist(
+    link_sub geometry,
+    link_main geometry)
+  RETURNS double precision AS
+$BODY$
+DECLARE
+	start_dist double precision;
+	end_dist double precision;
+	result double precision;
+BEGIN
+
+	start_dist :=
+		ST_Distance(
+			ST_StartPoint(ST_Transform(ST_GeometryN(link_main,1),32190)),
+			ST_ClosestPoint(ST_Transform(link_main,32190),ST_StartPoint(ST_Transform(ST_GeometryN(link_sub,1),32190)))
+			)
+			/
+			ST_Length(ST_Transform(link_main,32190));
+	end_dist :=
+		ST_Distance(
+			ST_StartPoint(ST_Transform(ST_GeometryN(link_main,1),32190)),
+			ST_ClosestPoint(ST_Transform(link_main,32190),ST_EndPoint(ST_Transform(ST_GeometryN(link_sub,ST_NumGeometries(link_sub)),32190)))
+			)
+			/
+			ST_Length(ST_Transform(link_main,32190));
+
+	result := LEAST(start_dist, end_dist);
+
+	RETURN result;
+END;
+$BODY$
+  LANGUAGE plpgsql IMMUTABLE
+  COST 100;
+ALTER FUNCTION line_dist(geometry, geometry)
+  OWNER TO aharpal;
