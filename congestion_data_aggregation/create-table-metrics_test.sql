511c5a6ca2dbde45ecec85c75e3bdf743dad6b4b
jovenc <joven.chew@toronto.ca>
2020-01-21 17:28:50 -0500

    #42 modified tti calculation

--- congestion_data_aggregation/create-table-metrics_test.sql
+++ congestion_data_aggregation/create-table-metrics_test.sql
@@ -1,8 +1,7 @@
---Using all data
 CREATE TABLE congestion.metrics_test AS
 
 SELECT *,
-avg_spd / baseline_spd AS tti,
+avg_tt / baseline_tt AS tti,
 (pct95_tt - avg_tt)/avg_tt AS bi
 
 FROM (
@@ -12,12 +11,11 @@ COUNT(datetime_bin) AS num_bins,
 b.seg_length, 
 AVG(a.segment_tt_avg_all) AS avg_tt,
 PERCENTILE_CONT (0.95) WITHIN GROUP (ORDER BY a.segment_tt_avg_all ASC) AS pct95_tt,
-b.seg_length / AVG(a.segment_tt_avg_all) * 3.6 AS avg_spd,
-b.spd_baseline_10pct_all AS baseline_spd
+b.tt_baseline_10pct_all AS baseline_tt
 FROM congestion.tt_segments_30min_test a
 LEFT JOIN congestion.tt_segments_baseline_test b
 USING (segment_id)
 WHERE a.segment_tt_avg_all IS NOT NULL
-GROUP BY segment_id, datetime_bin::time, b.seg_length, b.spd_baseline_10pct_all
+GROUP BY segment_id, datetime_bin::time, b.seg_length, b.tt_baseline_10pct_all
 ORDER BY segment_id, datetime_bin::time
 ) abc
