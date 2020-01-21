e4a48df5bb387c5bdd89cb139e3cb49e8da5c6cb
jovenc <joven.chew@toronto.ca>
2020-01-21 17:19:09 -0500

    #42 Fixed avg spd calculation

--- congestion_data_aggregation/create-table-metrics_test.sql
+++ congestion_data_aggregation/create-table-metrics_test.sql
@@ -1,17 +1,18 @@
+--Using all data
 CREATE TABLE congestion.metrics_test AS
 
-SELECT segment_id,
-time_bin,
+SELECT *,
 avg_spd / baseline_spd AS tti,
 (pct95_tt - avg_tt)/avg_tt AS bi
 
 FROM (
 SELECT a.segment_id, 
 a.datetime_bin::time AS time_bin, 
+COUNT(datetime_bin) AS num_bins, 
 b.seg_length, 
 AVG(a.segment_tt_avg_all) AS avg_tt,
 PERCENTILE_CONT (0.95) WITHIN GROUP (ORDER BY a.segment_tt_avg_all ASC) AS pct95_tt,
-b.seg_length / AVG(a.segment_tt_avg_all) AS avg_spd,
+b.seg_length / AVG(a.segment_tt_avg_all) * 3.6 AS avg_spd,
 b.spd_baseline_10pct_all AS baseline_spd
 FROM congestion.tt_segments_30min_test a
 LEFT JOIN congestion.tt_segments_baseline_test b
