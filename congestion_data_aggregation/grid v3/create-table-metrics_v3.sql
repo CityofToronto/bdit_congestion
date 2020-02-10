883f4fee628087cbec1c858a558b9191bb5724f1
jovenc <joven.chew@toronto.ca>
2020-02-10 16:31:32 -0500

    #42 rearraging queries for different grid version

---
+++ congestion_data_aggregation/grid v3/create-table-metrics_v3.sql
@@ -0,0 +1,25 @@
+--returned 587,145
+--took 47.5 s
+
+CREATE TABLE congestion.metrics_v3 AS
+
+SELECT segment_id, time_bin, num_bins,
+avg_tt, baseline_tt, pct95_tt,
+avg_tt / baseline_tt AS tti,
+(pct95_tt - avg_tt)/avg_tt AS bi
+
+FROM (
+SELECT a.segment_id, 
+a.datetime_bin::time AS time_bin, 
+COUNT(datetime_bin) AS num_bins, 
+b.seg_length, 
+AVG(a.segment_tt_avg_hc) AS avg_tt,
+PERCENTILE_CONT (0.95) WITHIN GROUP (ORDER BY a.segment_tt_avg_hc ASC) AS pct95_tt,
+b.tt_baseline_25pct AS baseline_tt
+FROM congestion.tt_segments_30min_v3 a
+LEFT JOIN congestion.tt_segments_baseline_v3 b
+USING (segment_id)
+WHERE a.segment_tt_avg_all IS NOT NULL
+GROUP BY segment_id, datetime_bin::time, b.seg_length, b.tt_baseline_25pct
+ORDER BY segment_id, datetime_bin::time
+) abc
