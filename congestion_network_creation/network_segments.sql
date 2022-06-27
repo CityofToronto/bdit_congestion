89ead3980ada5f2a3361335f226cc0ac54cc7637
chmnata <chmnata@gmail.com>
2022-06-27 14:51:20 -0400

    #56 add start and end vid to table

--- congestion_network_creation/network_segments.sql
+++ congestion_network_creation/network_segments.sql
@@ -1,11 +1,14 @@
 create table congestion.network_segments as 
 
-select segment_id, ST_linemerge(ST_union(geom)) as geom, 
-round(ST_length(ST_transform(ST_linemerge(ST_union(geom)), 2952))::numeric, 2) as total_length
+select  segment_id, 
+        start_vid, 
+        end_vid,
+        ST_linemerge(ST_union(geom)) as geom, 
+        round(ST_length(ST_transform(ST_linemerge(ST_union(geom)), 2952))::numeric, 2) as total_length
 
 from congestion.network_links_21_1
-group by segment_id
-order by segment_id;
+group by segment_id, start_vid, end_vid
+order by segment_id, start_vid, end_vid;
 
 COMMENT ON TABLE congestion.network_segments IS 
 'Containing all the segments in the congestion network.';
