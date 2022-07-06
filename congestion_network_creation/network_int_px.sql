ff1ffe646ccc15c582530c8684bf79eb0ddc01bc
chmnata <chmnata@gmail.com>
2022-07-06 11:44:19 -0400

    #57 update sql to exclude matches with further than 25m

--- congestion_network_creation/network_int_px.sql
+++ congestion_network_creation/network_int_px.sql
@@ -3,26 +3,20 @@
 
 CREATE TABLE congestion.network_int_px_21_1 AS 
 
-select  node_id, 
+SELECT  node_id, 
 		int_id, 
 		px, 
 		nodes.geom as node_geom,
 		ints.geom as ints_geom,
 		dist
 		
-from congestion.network_nodes nodes
-cross join lateral (select int_id, px, geom, ST_transform(ints.geom, 2952) <-> ST_transform(nodes.geom, 2952) as dist
-					 from congestion.selected_intersections ints
-					 order by (ints.geom <-> nodes.geom)
+FROM congestion.network_nodes nodes
+CROSS JOIN LATERAL ( SELECT int_id, px, geom, ST_transform(ints.geom, 2952) <-> ST_transform(nodes.geom, 2952) AS dist
+					 FROM congestion.selected_intersections ints
+					 ORDER BY (ints.geom <-> nodes.geom)
 					 LIMIT 1)ints
-
-order by dist desc;
+WHERE dist < 25
+ORDER BY dist desc;
 
 COMMENT ON TABLE congestion.network_int_px_21_1
     IS 'Lookup table containing nodes used in routing network and its equivalent centreline intersection id and px. Based on 21_1 map version nodes. ';
-    
-    
--- Ended up deleting matched nodes and ints where the distance between them were more than 25m
--- Next time should use a buffer when matching nodes to ints
-DELETE FROM congestion.network_int_px_21_1
-where dist > 25
