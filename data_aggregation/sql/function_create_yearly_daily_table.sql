d0805606edcb9d5f6d1bae337bfd3c121eb50c5a
chmnata <chmnata@gmail.com>
2022-08-31 12:32:12 -0400

    #61 modify function to create daily table to monthly instead of yearly

--- data_aggregation/sql/function_create_yearly_daily_table.sql
+++ data_aggregation/sql/function_create_yearly_daily_table.sql
@@ -10,12 +10,19 @@ DECLARE
 	enddate DATE;
 	basetablename TEXT := 'network_segments_daily_';
 	tablename TEXT;
-BEGIN
+	yyyymm TEXT;
 
-	startdate:= to_date(yyyy||'-01-01', 'YYYY-MM-DD');
-	enddate:= startdate + INTERVAL '1 year';
-	tablename:= basetablename||yyyy;
-    EXECUTE format($$CREATE TABLE congestion.%I 
+	BEGIN
+    FOR mm IN 01..12 LOOP
+        startdate:= to_date(yyyy||'-'||mm||'-01', 'YYYY-MM-DD');
+		enddate:= startdate + INTERVAL '1 month';
+        IF mm < 10 THEN
+            yyyymm:= yyyy||'0'||mm;
+        ELSE
+            yyyymm:= yyyy||''||mm;
+        END IF;
+        tablename:= basetablename||yyyymm;
+    	EXECUTE format($$CREATE TABLE congestion.%I 
                      PARTITION OF congestion.network_segments_daily
                      FOR VALUES FROM  (%L) TO (%L);
                      CREATE INDEX ON congestion.%I  (segment_id);
@@ -25,6 +32,7 @@ BEGIN
                      ALTER TABLE congestion.%I OWNER TO congestion_admins;
                      $$
                      , tablename, startdate, enddate, tablename, tablename, tablename, tablename, tablename);
+	END LOOP;
 END;
 $BODY$;
 
