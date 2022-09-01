00cdb381f487dd6f018b610d1c852eb1f9224f86
chmnata <chmnata@gmail.com>
2022-09-01 11:51:39 -0400

    #60 Add routing function sql

---
+++ congestion_network_creation/network_segment_routing.sql
@@ -0,0 +1,17 @@
+CREATE MATERIALIZED VIEW congestion.network_segment_routing AS
+
+SELECT  segment_id,
+        start_vid::integer AS source,
+        end_vid::integer AS target,
+        total_length::integer AS cost,
+        geom
+
+FROM    congestion.network_segments;
+
+ALTER TABLE congestion.network_segment_routing
+    OWNER TO congestion_admins;
+
+COMMENT ON MATERIALIZED VIEW congestion.network_segment_routing
+    IS 'Routing network for network segments. Contains source, target and cost for each segment in the congestion network.';
+
+GRANT SELECT ON TABLE congestion.network_segment_routing TO bdit_humans;
