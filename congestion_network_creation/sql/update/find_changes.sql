abe414c6d857ba3f650b66363da6b377d136b970
chmnata <chmnata@gmail.com>
2022-09-06 15:49:37 -0400

    #63 Update find changes sql

--- congestion_network_creation/sql/update/find_changes.sql
+++ congestion_network_creation/sql/update/find_changes.sql
@@ -26,3 +26,11 @@ with changed_links AS (
 	
 select distinct segment_id from  congestion.network_links_21_1 
 inner join changed_links using (link_dir)
+
+
+-- First find the segment_ids affected by node_id changes
+-- returned 0 in this case, because those nodes were not used 
+select * from congestion.network_segments
+inner join (select node_id from congestion.network_nodes
+			except 
+			select node_id from here.routing_nodes_22_2 )a on start_vid = node_id or end_vid = node_id
