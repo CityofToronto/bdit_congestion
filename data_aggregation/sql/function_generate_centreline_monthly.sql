4ab8ebe48dc231e098a5f07732541591b766d16f
chmnata <chmnata@gmail.com>
2022-10-12 20:45:22 -0400

    #66 update centreline monthly travel time function

--- data_aggregation/sql/function_generate_centreline_monthly.sql
+++ data_aggregation/sql/function_generate_centreline_monthly.sql
@@ -14,29 +14,34 @@ AS $BODY$
 	SELECT  	uid, 
                 dt, 
                 hr, 
-                round(length / (sum(total_length) / sum(tt)), 2) as centreline_daily_tt, 
+				((segment_set_length / (sum(total_length) / sum(tt))) * cent_length)/segment_set_length as tt,
                 baseline_10pct, 
                 baseline_25pct
 	
     FROM        congestion.network_segments_daily
 	INNER JOIN  congestion.segment_centreline_lookup using (segment_id)
+	INNER JOIN  congestion.network_segments using (segment_id)
     LEFT JOIN   congestion.centreline_baseline using (uid)
-	WHERE       (dt >= _dt AND dt < _dt + INTERVAL '1 month') AND yr = EXTRACT(YEAR From _dt)
+	LEFT JOIN 	ref.holiday USING (dt) -- exclude holidays
+	WHERE       (dt >= _dt AND dt < _dt + INTERVAL '1 month') AND 
+				yr::int = EXTRACT(YEAR From _dt) AND
+				holiday.dt IS NULL 
         
-	GROUP BY uid, dt, hr, length)
+	GROUP BY 	uid, dt, hr, total_length, baseline_10pct, baseline_25pct, segment_set_length, cent_length
+	HAVING 		sum(total_length) >= (segment_set_length * 0.8))
     
     SELECT 		uid, 
 				date_trunc('month', a.dt) AS mth, 
 				hr,
 				CASE WHEN extract(isodow from dt) <6 then 'Weekday'
 					ELSE 'Weekend' END AS day_type,
-				round(avg(centreline_daily_tt), 2) AS avg_tt,
-				PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY centreline_daily_tt) AS median_tt,
-				PERCENTILE_CONT(0.85) WITHIN GROUP (ORDER BY centreline_daily_tt) AS pct_85_tt,
-				PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY centreline_daily_tt) AS pct_95_tt,
-				round(min(centreline_daily_tt), 2) AS min_tt,
-				round(max(centreline_daily_tt), 2) AS max_tt,
-				stddev(centreline_daily_tt) AS std_dev,
+				round(avg(tt), 2) AS avg_tt,
+				PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY tt) AS median_tt,
+				PERCENTILE_CONT(0.85) WITHIN GROUP (ORDER BY tt) AS pct_85_tt,
+				PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY tt) AS pct_95_tt,
+				round(min(tt), 2) AS min_tt,
+				round(max(tt), 2) AS max_tt,
+				stddev(tt) AS std_dev,
                 baseline_10pct AS py_pct_10_tt,
                 baseline_25pct AS py_pct_25_tt
     
@@ -44,8 +49,8 @@ AS $BODY$
 	LEFT JOIN 	ref.holiday USING (dt) -- exclude holidays
     WHERE 		holiday.dt IS NULL 
     
-	GROUP BY    uid, mth, hr, day_type, baseline_10pct
-	ORDER BY    uid, mth, hr, day_type, baseline_25pct
+	GROUP BY    uid, mth, hr, day_type, baseline_10pct, baseline_25pct
+	ORDER BY    uid, mth, hr, day_type
 	
 
 $BODY$;
