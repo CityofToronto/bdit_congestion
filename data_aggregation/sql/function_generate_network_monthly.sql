0d4b0a9f59e49d574edb000a42d93cdfa01aec8b
chmnata <chmnata@gmail.com>
2022-10-26 10:49:31 -0400

    #68, #61 Update segment monthly table to use is_valid column

--- data_aggregation/sql/function_generate_network_monthly.sql
+++ data_aggregation/sql/function_generate_network_monthly.sql
@@ -24,8 +24,9 @@ AS $BODY$
     
     FROM  		congestion.network_segments_daily
 	LEFT JOIN 	ref.holiday USING (dt) -- exclude holidays
-    WHERE 		(dt >= _dt AND dt < _dt + INTERVAL '1 month') and 
-				holiday.dt IS NULL 
+    WHERE 		(dt >= _dt AND dt < _dt + INTERVAL '1 month') AND 
+				holiday.dt IS NULL AND
+                valid IS true
     
 	GROUP BY    segment_id, mth, hr, day_type
 	ORDER BY    segment_id, mth, hr, day_type
