d8692a2e1f7bd445cb12a1591738bde47a279d38
chmnata <chmnata@gmail.com>
2025-06-02 16:38:03 +0000

    #82 update dag to include monthly partitions tables func

--- dags/generate_congestion_agg.py
+++ dags/generate_congestion_agg.py
@@ -2,8 +2,8 @@
 airflow_congestion_aggregation
 
 A data aggregation workflow that aggregates travel time data for
-three Congestion Network intermediate tables, network_daily, network_monthly,
-and centreline_monthly. This DAG is schedule to run only when pull_here DAG
+three Congestion Network intermediate tables, network_daily_spd. 
+This DAG is schedule to run only when pull_here DAG
 finished running. 
 """
 import os
@@ -11,12 +11,16 @@ import sys
 import logging
 from datetime import datetime, timedelta
 
-from airflow.decorators import dag, task
+from airflow.decorators import dag, task, task_group
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator, SQLCheckOperator
 
-file_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
-sys.path.insert(0, file_path)
-from bdit_dag_utils.utils.dag_functions import task_fail_slack_alert
+try:
+    repo_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    sys.path.insert(0, repo_path)
+    from bdit_dag_utils.utils.dag_functions import task_fail_slack_alert
+    from bdit_dag_utils.utils.common_tasks import check_jan_1st, check_1st_of_month
+except:
+    raise ImportError("Cannot import slack alert functions")
 
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -38,31 +42,30 @@ default_args = {'owner':'natalie',
     default_args=default_args, 
     schedule=None, # triggered by here dag 
     catchup=False,
-    tags=["HERE"]
+    tags=["HERE", "aggregation"]
 )
 def congestion_aggregation():
-    
-    ## Tasks ##
-    
-    ## ShortCircuitOperator Tasks, python_callable returns True or False; False means skip downstream tasks
-    @task.short_circuit()
-    def check_dom(ds=None):
-        execution_date = datetime.strptime(ds, "%Y-%m-%d") - timedelta(days=1)
-        return execution_date.day == 1
+    @task_group(tooltip="Tasks to check if necessary to create new partitions and if so, exexcute.")
+    def check_partitions():
 
-    ## SQLCheckOperator to check if all of last weeks data is in the data before aggregating
-    check_monthly = SQLCheckOperator(
-        task_id = 'check_monthly',
-        conn_id = 'congestion_bot',
-        sql = '''SELECT case when count(distinct dt) = 
-                    extract('days' FROM (('{{ ds }}'::date - 1) - ('{{ ds }}'::date - 1 - interval '1 month'))
-                    THEN TRUE ELSE FALSE END AS counts
-            FROM here.ta
-            WHERE
-                dt >= '{{ ds }}'::date - 1 - interval '1 month'
-                AND dt < '{{ ds }}'::date - 1'''
-    )
+        create_annual_partition = SQLExecuteQueryOperator(
+            task_id='create_annual_partitions',
+            pre_execute=check_jan_1st,
+            sql=["SELECT congestion.create_yyyy_partition('{{ macros.ds_format(ds, '%Y-%m-%d', '%Y') }}'::int)"],
+            conn_id='congestion_bot',
+            autocommit=True
+        )
+      
+        create_month_partition = SQLExecuteQueryOperator(
+            task_id='create_month_partition',
+            pre_execute=check_1st_of_month,
+            sql="""SELECT congestion.create_yyyymm_partitions('{{ macros.ds_format(ds, '%Y-%m-%d', '%Y') }}'::int, '{{ macros.ds_format(ds, '%Y-%m-%d', '%m') }}'::int)""",
+            conn_id='congestion_bot',
+            autocommit=True,
+            trigger_rule='none_failed'
+        )
 
+        create_annual_partition >> create_month_partition
     ## Postgres Tasks
     # Task to aggregate segment level tt daily
     aggregate_daily = SQLExecuteQueryOperator(
@@ -71,26 +74,9 @@ def congestion_aggregation():
         conn_id='congestion_bot',
         autocommit=True,
         retries = 0)
+    
+    # Data checks TBD 
 
-
-    # Task to aggregate segment level tt monthly
-    aggregate_seg_monthly = SQLExecuteQueryOperator(
-        sql='''select congestion.generate_network_monthly('{{ ds }}'::date - 1 - interval '1 month');''',
-        task_id='aggregate_seg_monthly',
-        conn_id='congestion_bot',
-        autocommit=True,
-        retries = 0
-    )
-
-    # Task to aggregate centreline level tt monthly 
-    aggregate_cent_monthly = SQLExecuteQueryOperator(
-        sql='''select congestion.generate_centreline_monthly('{{ ds }}'::date - 1 - interval '1 month');''',
-        task_id='aggregate_cent_monthly',
-        conn_id='congestion_bot',
-        autocommit=True,
-        retries = 0,
-    )
-
-    aggregate_daily >> check_dom() >> check_monthly >> [aggregate_seg_monthly, aggregate_cent_monthly]
+    check_partitions() >> aggregate_daily
 
 congestion_aggregation()
