bb8a35d8a26c81d61428081ad098c1ebfcb87f93
chmnata <chmnata@gmail.com>
2026-04-13 15:41:19 +0000

    #i92 add centreline version to input for highway function

--- congestion_network_creation/sql/update/function_update_congestion_highway.sql
+++ congestion_network_creation/sql/update/function_update_congestion_highway.sql
@@ -3,11 +3,13 @@
 
 CREATE OR REPLACE FUNCTION congestion.update_congestion_highway(
     table_to_update text,
-    centreline_lookup text
+    centreline_lookup text,
+    centreline_version text
 )
 RETURNS integer AS $$
 DECLARE
     v_rows_affected integer;
+	centreline_version_table text:= 'centreline_'||centreline_version;
 BEGIN
     EXECUTE format(
         'UPDATE congestion.%I
@@ -22,13 +24,14 @@ BEGIN
                     unnest(centreline_ids) AS centreline_id
                 FROM congestion.%I
             ) unnested
-            INNER JOIN gis_core.centreline_latest USING (centreline_id)
+            INNER JOIN gis_core.%I USING (centreline_id)
             GROUP BY segment_id
             HAVING min(feature_code) = 201100
          ) a
          WHERE a.segment_id = %I.segment_id
          AND %I.highway IS DISTINCT FROM a.highway_tf',
         table_to_update,
+		centreline_version_table,
         centreline_lookup,
         table_to_update,
         table_to_update
