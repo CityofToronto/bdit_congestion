b63f53ddd3dd0c53ef665fce952bd9ad1088173c
chmnata <chmnata@gmail.com>
2026-04-15 18:56:42 +0000

    #92 add old_ver to input and update to use intersection layer

--- congestion_network_creation/sql/update/function_create_temp_int_px_nodes.sql
+++ congestion_network_creation/sql/update/function_create_temp_int_px_nodes.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION congestion.create_temp_int_px_nodes(ver_id TEXT)
+CREATE OR REPLACE FUNCTION congestion.create_temp_int_px_nodes(old_ver_id TEXT, new_ver_id TEXT, centreline_ver text)
 RETURNS TABLE (
     px INT,
     px_geom GEOMETRY,
@@ -8,11 +8,11 @@ RETURNS TABLE (
     node_geom GEOMETRY
 ) AS $$
 DECLARE
-    table_name TEXT;
-    routing_nodes_table TEXT;
+    table_name text := 'temp_int_px_nodes_' || new_ver_id;
+    routing_nodes_table text := 'routing_nodes_' || new_ver_id;
+    centreline_name text := 'intersection_'|| new_ver_id; 
+	lookup text := 'congestion_nodes_lookup_' || old_ver_id;
 BEGIN
-    table_name := 'temp_int_px_nodes_' || ver_id;
-    routing_nodes_table := 'here.routing_nodes_' || ver_id;
 
     EXECUTE format(
         'CREATE TABLE congestion.%I AS
@@ -22,7 +22,7 @@ BEGIN
             LEFT JOIN (
                 SELECT px::INT FROM congestion.excluded_signals
                 UNION 
-                SELECT px::INT FROM congestion.int_px_nodes_latest
+                SELECT px::INT FROM congestion.%I
             ) old_signal ON traffic_signal.px::INT = old_signal.px::INT
             WHERE removed_date IS NULL 
               AND temp_signal IS NULL 
@@ -33,7 +33,7 @@ BEGIN
             FROM new_signals signals 
             CROSS JOIN LATERAL ( 
                 SELECT px, signals.geom AS px_geom, intersection_id, ints.geom AS int_geom
-                FROM gis_core.intersection_latest ints 
+                FROM gis_core.%I ints 
                 ORDER BY ST_TRANSFORM(signals.geom, 2952) <-> ST_TRANSFORM(ints.geom, 2952)
                 LIMIT 1
             ) AS ints
@@ -46,12 +46,16 @@ BEGIN
             nodes.node_id,
             nodes.geom AS node_geom
         FROM int_joins
-        LEFT JOIN %I nodes ON 
+        LEFT JOIN here.%I nodes ON 
             ST_DWITHIN(ST_TRANSFORM(int_joins.int_geom, 2952), ST_TRANSFORM(nodes.geom, 2952), 25
             )',
         table_name,
+		lookup,
+		centreline_name,
         routing_nodes_table
     );
     
 END;
 $$ LANGUAGE plpgsql;
+
+alter function congestion.create_temp_int_px_nodes(old_ver_id TEXT, new_ver_id TEXT, centreline_ver text) owner to congestion_admins;
