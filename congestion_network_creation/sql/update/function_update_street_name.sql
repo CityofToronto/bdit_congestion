abacfc4effdf9e1f4be98076b62c4455cad7a655
chmnata <chmnata@gmail.com>
2026-05-15 19:35:12 +0000

    #92 make int name distinctgit add congestion_network_creation/sql/update/function_update_street_name.sql

--- congestion_network_creation/sql/update/function_update_street_name.sql
+++ congestion_network_creation/sql/update/function_update_street_name.sql
@@ -11,15 +11,23 @@ DECLARE
     centreline_table text := 'centreline_' || centreline_ver;
 BEGIN
 	EXECUTE format('with all_ints as (
-		select distinct intersection_id, linear_name_full, feature_code
+		select  intersection_id, linear_name_full, min(feature_code) as feature_code
 		from ( select from_intersection_id AS intersection_id, linear_name_full, feature_code
 				from gis_core.%I
 				union
 				select to_intersection_id, linear_name_full, feature_code
 				from gis_core.%I
-				)b)
-		
-		, get_road_name as (
+				)b
+		group by intersection_id, linear_name_full
+			)
+, all_ints_agg as (
+    select 
+        intersection_id,
+        array_agg(linear_name_full order by feature_code)::text[] as int_names
+    from all_ints
+    group by intersection_id
+)		
+, get_road_name as (
 		select segment_id, from_int, to_int,
 		array_agg(distinct linear_name_full)::text[] centreline_list
 		FROM (	SELECT segment_id, from_int, to_int, unnest(centreline_ids) as centreline_id
@@ -28,24 +36,24 @@ BEGIN
 		left join gis_core.%I using (centreline_id)
 		group by segment_id, from_int, to_int)
 		
-		, get_intersection_name as (
+, get_intersection_name as (
 		select 
 			segment_id, from_int,
-			array_agg(a.linear_name_full order by a.feature_code)::text[] as from_name,
-			array_agg(b.linear_name_full order by b.feature_code)::text[] as to_name,
+			a.int_names as from_name,
+        	b.int_names as to_name,
 			to_int, centreline_list
 		from get_road_name
-		left join all_ints a on from_int = a.intersection_id
-		left join all_ints b on to_int = b.intersection_id			
-		group by segment_id, from_int, to_int, centreline_list)
-		
-		, final_output AS (
-		SELECT 
-			segment_id, 
-			from_int,
-			case when (array_diff(from_name, centreline_list))[1] is null then array_to_string(from_name, ''/'')
-				else array_to_string(array[(array_diff(from_name, centreline_list))[1]], ''/'') end as from_name_final,
-			to_int,
+		left join all_ints_agg a on from_int = a.intersection_id
+		left join all_ints_agg b on to_int = b.intersection_id	
+	)
+	, final_output AS (	
+SELECT segment_id, 
+		from_int,
+		case when (array_diff(from_name, centreline_list))[1] is null 
+				then array_to_string(from_name, ''/'') 
+			else array_to_string(array[(array_diff(from_name, centreline_list))[1]], ''/'') end as from_name_final,
+
+		to_int,
 			case when (array_diff(to_name, centreline_list))[1] is null then array_to_string(to_name, ''/'')
 				else array_to_string(array[(array_diff(to_name, centreline_list))[1]], ''/'') end as to_name_final,
 				array_to_string(centreline_list, ''/'') AS centreline_name
