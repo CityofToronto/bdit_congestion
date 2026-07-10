c687c0bdda0a643bfaf28c4a81b82fc3015ca003
chmnata <chmnata@gmail.com>
2026-07-10 19:44:32 +0000

    #92 update routing function

--- congestion_network_creation/sql/routing/function_get_segments_btwn_nodes.sql
+++ congestion_network_creation/sql/routing/function_get_segments_btwn_nodes.sql
@@ -1,32 +1,58 @@
-CREATE OR REPLACE FUNCTION congestion.get_segments_btwn_nodes(
-    start_node integer,
-    end_node integer,
-    OUT start_node integer,
-    OUT end_node integer,
-    OUT segment_list int [],
-    OUT length numeric,
-    OUT geom geometry
-)
-RETURNS record
-LANGUAGE sql
-COST 100
-STABLE STRICT PARALLEL UNSAFE
-AS $BODY$
-WITH results as (
-	SELECT * 
-	FROM pgr_dijkstra('SELECT segment_id as id, source::int, target::int,cost from congestion.network_segment_routing', start_node, end_node)
-)
+-- FUNCTION: congestion.get_congestion_segments_btwn_nodes(integer, integer, text)
 
-SELECT start_node, end_node, array_agg(segment_id::int), round(sum(ST_length(ST_transform(geom, 2952)))::numeric,2) as length, ST_union(ST_linemerge(geom)) as geom
-from   results
-inner join congestion.network_segments on edge=segment_id
+-- DROP FUNCTION IF EXISTS congestion.get_congestion_segments_btwn_nodes(integer, integer, text);
+
+CREATE OR REPLACE FUNCTION congestion.get_congestion_segments_btwn_nodes(
+	start_vid integer,
+	end_vid integer,
+	ver_id text DEFAULT NULL::text,
+	OUT start_node integer,
+	OUT end_node integer,
+	OUT segment_list integer[],
+	OUT length numeric,
+	OUT geom geometry)
+    RETURNS record
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+DECLARE
+    congestion_table text := format('congestion_segments_%s', ver_id);
+BEGIN  
+   EXECUTE format($f$
+        WITH results AS (
+            SELECT *
+            FROM pgr_dijkstra(
+                'SELECT segment_id AS id,
+                        start_vid::integer AS source,
+                        end_vid::integer AS target,
+                        total_length::integer AS cost
+                 FROM congestion.%1$I', %2$L::int, %3$L::int )
+        )
+        SELECT
+            %2$L AS start_node,
+            %3$L AS end_node,
+            array_agg(segment_id::int ORDER BY path_seq),
+            round(sum(ST_length(ST_transform(geom, 2952)))::numeric, 2) AS length,
+            ST_union(ST_linemerge(geom)) AS geom
+        FROM results
+        INNER JOIN congestion.%1$I ON edge = segment_id
+		GROUP BY start_node, end_node
+    $f$, congestion_table, start_vid, end_vid)
+	INTO start_node, end_node, segment_list, length, geom;
+	RETURN;
+
+END;
 $BODY$;
 
-COMMENT ON FUNCTION congestion.get_segments_btwn_nodes(integer, integer)
-IS 'Function created for routing segments in the network_segments using here nodes.  ';
+ALTER FUNCTION congestion.get_congestion_segments_btwn_nodes(integer, integer, text)
+    OWNER TO congestion_admins;
 
-ALTER FUNCTION congestion.get_segments_btwn_nodes(integer, integer)
-OWNER TO congestion_admins;
+GRANT EXECUTE ON FUNCTION congestion.get_congestion_segments_btwn_nodes(integer, integer, text) TO PUBLIC;
 
+GRANT EXECUTE ON FUNCTION congestion.get_congestion_segments_btwn_nodes(integer, integer, text) TO bdit_humans;
 
-GRANT EXECUTE ON FUNCTION congestion.get_segments_btwn_nodes(integer, integer) TO bdit_humans;
+GRANT EXECUTE ON FUNCTION congestion.get_congestion_segments_btwn_nodes(integer, integer, text) TO congestion_admins;
+
+COMMENT ON FUNCTION congestion.get_congestion_segments_btwn_nodes(integer, integer, text)
+    IS 'Functions to input start and end node_id and return the congestion segments in between those two input.';
